5699bd99892acec313146cc38daaac40e27a8cf3
SeregadD90 <seregad07@mail.ru>
2023-02-24 16:40:11 +0700

    класс с описанием координат x и y

--- units/Point2D.java
+++ units/Point2D.java
@@ -8,10 +8,12 @@ public class Point2D {
         this.y = y;
     }
 
+
+// *нахождение расстояния между персонажами */
+
     public double Distance(Point2D targetCoordinate) {
         return Math.sqrt(Math.pow(this.x - targetCoordinate.x, 2) + Math.pow(this.y - targetCoordinate.y, 2));
     }
   
-
 
 }
